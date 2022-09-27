6fece4fac16e4a511c94f4a0728974a9f549562d
tadanomizu <e1b20085@oit.ac.jp>
2022-09-27 16:04:48 +0900

    リポジトリの初期化

---
+++ src/test/java/oit/is/z1358/kaizi/janken/JankenApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.z1358.kaizi.janken;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JankenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
